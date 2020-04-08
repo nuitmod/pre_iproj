a5eb913ea1a2d89ca4dc022eea325ecbae7d514f
nuitmod <wtvfqm@gmail.com>
2020-04-08 17:37:07 +0500

    add preacthtm

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 'use strict';
 //from window.preact import preact;
-// { h, Component } from 'preact'
+//import { html, render, Component } from "./node_modules/htm/preact";
 //import { h, render} from preact';
 
-/** @jsx h */
+///** @jsx h */
 
 //console.log(window.preact);
 
-var {h, render, Component}=preact;
+var {html, render, Component}=htmPreact;
+//var htm = window.htm;
+//console.log(html);
 
-class Ww extends Component{
-  render(){
-    return(
-      <div>ww component data</div>
-    )
-  }
+var I_ww=function(){
+  return html`
+    <div>
+      <h6>ww component data</h6>
+    </div>
+  `
 }
 
-render(<Ww />, document.getElementById("root2"));
+
+render(I_ww(), document.getElementById("root"))
+
+
+//var html=htm.bind(h)
+/*
+var Ww = function(){
+    return h('h6', {className: ''},
+    'ww component data');
+}
+//render(Ww(), document.getElementById("root2"));
+
+var App = function (){
+  return h(Ww)}
+
+render(h(App), document.getElementById("root2"));
+
+//render(<Ww />, document.getElementById("root2"));
 /*
 var render_app= () => {
   render(
@@ -25,7 +44,6 @@ var render_app= () => {
     document.getElementById("root2")
   )
 }
-*/
 
 function Icomp(){
   return (
@@ -34,6 +52,6 @@ function Icomp(){
     </div>
   )
 }
-
+*/
 //render_app()
-render(<Icomp />, document.getElementById("root3"))
+//render(<Icomp />, document.getElementById("root3"))
